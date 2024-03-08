9f0445cf1acfc79993adbdc1e55fe2c736183171
Marcin Hagmajer <mhagmajer@gmail.com>
2024-03-08 01:47:26 +0100

    feat(frontend): show spinner when loading transactions

--- frontend/src/App/TransactionFeed.tsx
+++ frontend/src/App/TransactionFeed.tsx
@@ -1,6 +1,7 @@
 import type { AccountBase, Transaction } from 'plaid';
 import React, { useCallback } from 'react';
 import { Button } from '../components/Button';
+import { LoadingSpinner } from '../components/LoadingSpinner';
 import { useGetAccounts } from '../hooks/useGetAccounts';
 import { useGetItem } from '../hooks/useGetItem';
 import { useGetTransactions } from '../hooks/useGetTransactions';
@@ -15,26 +16,35 @@ export const TransactionFeed: React.FC = () => {
   }, [getTransactions]);
 
   if (getTransactions.isLoading || getAccounts.isLoading || getItem.isLoading) {
-    return <div>loading...</div>;
+    return <LoadingSpinner />;
   }
 
   if (!getTransactions.data || !getAccounts.data || !getItem.data) {
     return <div>failed to load</div>;
   }
 
+  const { accounts } = getAccounts.data;
   const { institution } = getItem.data;
 
   return (
     <div className="container mx-auto space-y-4">
       <div className="flex items-center justify-between bg-white shadow-lg rounded-lg p-4">
-        <div className="text-xl">{institution.name}</div>
+        <div className="flex flex-col">
+          <span className="font-bold text-xl">{institution.name}</span>
+          <span className="text-sm text-gray-500">
+            {[
+              `${accounts.length} linked account`,
+              accounts.length !== 1 ? 's' : '',
+            ].join('')}
+          </span>
+        </div>
         <Button onClick={onRefresh} className="font-normal">
           Refresh transactions
         </Button>
       </div>
       <ul className="space-y-4">
         {getTransactions.data.latest_transactions.map((transaction) => {
-          const [account] = getAccounts.data!.accounts.filter(
+          const [account] = accounts.filter(
             (acc) => acc.account_id === transaction.account_id
           );
           return (
